d84908993bd1a07add05b3e0d736221b5b375949
Nathan <44456832+thaniel-c@users.noreply.github.com>
2020-03-31 20:27:37 -0400

    Update settings.py

--- copypasta/copypasta/settings.py
+++ copypasta/copypasta/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6vcvhgbr(uidd_w8)0&tdt2_^)-#!kg4hj+2lnqy&r(o#2lsty'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
