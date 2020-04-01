041f22e6e95604e4bac2af413782e2b3d06909aa
thaniel-c <contactlampshadecad@gmail.com>
2020-03-31 22:00:38 -0400

    data commit

--- copypasta/main/models.py
+++ copypasta/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length = 50)
+    content = models.TextField()
+    nsfw = models.BooleanField()
+    link = models.CharField(max_length = 50)
+    
+    def __str__(self):
+        return self.title
--- copypasta/main/views.py
+++ copypasta/main/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from bs4 import BeautifulSoup
+from .models import Post
+import json
 
+post_data = json.load(open("json/data.json"))
+print(post_data)
+
+posts = [
+
+    {
+        'title': 'TestTestTest',
+        'content': "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget tortor non sapien posuere porta sed a leo. Aenean mattis est venenatis nibh maximus, at convallis risus ornare. Praesent tristique risus sed vulputate euismod. Quisque sed vehicula dolor. Phasellus faucibus, odio sit amet commodo blandit, elit turpis dapibus metus, nec tristique tellus risus quis tortor. Pellentesque sodales suscipit felis at posuere. Donec sed elit id felis rutrum dictum sit amet ac dolor.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget tortor non sapien posuere porta sed a leo. Aenean mattis est venenatis nibh maximus, at convallis risus ornare. Praesent tristique risus sed vulputate euismod. Quisque sed vehicula dolor. Phasellus faucibus, odio sit amet commodo blandit, elit turpis dapibus metus, nec tristique tellus risus quis tortor. Pellentesque sodales suscipit felis at posuere. Donec sed elit id felis rutrum dictum sit amet ac dolor.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget tortor non sapien posuere porta sed a leo. Aenean mattis est venenatis nibh maximus, at convallis risus ornare. Praesent tristique risus sed vulputate euismod. Quisque sed vehicula dolor. Phasellus faucibus, odio sit amet commodo blandit, elit turpis dapibus metus, nec tristique tellus risus quis tortor. Pellentesque sodales suscipit felis at posuere. Donec sed elit id felis rutrum dictum sit amet ac dolor.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget tortor non sapien posuere porta sed a leo. Aenean mattis est venenatis nibh maximus, at convallis risus ornare. Praesent tristique risus sed vulputate euismod. Quisque sed vehicula dolor. Phasellus faucibus, odio sit amet commodo blandit, elit turpis dapibus metus, nec tristique tellus risus quis tortor. Pellentesque sodales suscipit felis at posuere. Donec sed elit id felis rutrum dictum sit amet ac dolor.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget tortor non sapien posuere porta sed a leo. Aenean mattis est venenatis nibh maximus, at convallis risus ornare. Praesent tristique risus sed vulputate euismod. Quisque sed vehicula dolor. Phasellus faucibus, odio sit amet commodo blandit, elit turpis dapibus metus, nec tristique tellus risus quis tortor. Pellentesque sodales suscipit felis at posuere. Donec sed elit id felis rutrum dictum sit amet ac dolor.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget tortor non sapien posuere porta sed a leo. Aenean mattis est venenatis nibh maximus, at convallis risus ornare. Praesent tristique risus sed vulputate euismod. Quisque sed vehicula dolor. Phasellus faucibus, odio sit amet commodo blandit, elit turpis dapibus metus, nec tristique tellus risus quis tortor. Pellentesque sodales suscipit felis at posuere. Donec sed elit id felis rutrum dictum sit amet ac dolor.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget tortor non sapien posuere porta sed a leo. Aenean mattis est venenatis nibh maximus, at convallis risus ornare. Praesent tristique risus sed vulputate euismod. Quisque sed vehicula dolor. Phasellus faucibus, odio sit amet commodo blandit, elit turpis dapibus metus, nec tristique tellus risus quis tortor. Pellentesque sodales suscipit felis at posuere. Donec sed elit id felis rutrum dictum sit amet ac dolor.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget tortor non sapien posuere porta sed a leo. Aenean mattis est venenatis nibh maximus, at convallis risus ornare. Praesent tristique risus sed vulputate euismod. Quisque sed vehicula dolor. Phasellus faucibus, odio sit amet commodo blandit, elit turpis dapibus metus, nec tristique tellus risus quis tortor. Pellentesque sodales suscipit felis at posuere. Donec sed elit id felis rutrum dictum sit amet ac dolor."
+    }
+]
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'main/index.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'main/index.html', context)
